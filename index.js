555d0d024da31a4ea1c3cb9d4eb15e2cee46d633
a7cha <akuarung83@gmail.com>
2020-11-24 18:50:49 +0700

    ini socket io

--- index.js
+++ index.js
@@ -7,15 +7,41 @@ const publicIp = require('public-ip');
 const internalIp = require('internal-ip');
 const admin = require('firebase-admin');
 const serviceAccount = require('./helloworld/zwalleet-1337-firebase-adminsdk-7hhof-036fe161da.json')
+const db = require('./src/helper/db')
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+
 // const db = require("./src/helper/db");
 const AuthRoute = require("./src/routes/Auth");
 const UserRoute = require("./src/routes/User");
 const TransactionRoute = require("./src/routes/Transaction");
 const TopupRoute = require("./src/routes/Topup");
 
-app.use(cors()); // WAJIB DI ISI
+app.use('*',cors()); // WAJIB DI ISI
+
+io.on('connection', socket => {
+  console.log('user connect')
+
+  socket.on('getId', id  => { 
+    if(id){
+      socket.join(id)
+      console.log('datanya adalah ini : ', id)
+      db.query(`SELECT balance FROM user WHERE id = ${id}`, (err, res) => {
+        io.to(id).emit('get-data', res[0].balance)
+        console.log(res[0].balance)
+      });      
+    }
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnect')
+  })
+})
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -32,36 +58,22 @@ app.use("/zwallet/api/v1/topup", TopupRoute);
 
 
 
-app.listen(process.env.PORT, () => {
+
+server.listen(process.env.PORT, () => {
 admin.initializeApp({
 	credential : admin.credential.cert(serviceAccount),
 	databaseURL : 'https://zwalleet-1337.firebaseio.com'
-}, console.log(admin.credential.cert(serviceAccount)));
+});
 
 
-const registrationToken = 'cVmVqUHCRT-hVq75otPmT5:APA91bH7u_cDF8rK5XWvKjCzUrfcYT6FJdaGEg4yWzCVzFqVLCIVcOklTHFnOhTpAsAAW0beEDbM-OzEr4IEJyfTh9L9C4ckQ0pfSwknHUyKhV9lcqyG5KTdr02g75QWgSksCkSLd4MW'
 
-var payload = {
-  notification: {
-    title: 'Zwallet',
-    body: '$FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.'
-  }
-};	
-
-admin.messaging().sendToDevice(registrationToken, payload)
-  .then(function(response) {
-    // See the MessagingDevicesResponse reference documentation for
-    // the contents of response.
-    console.log('Successfully sent message:', response);
-  })
-  .catch(function(error) {
-    console.log('Error sending message:', error);
-  });
 
   (async () => {
     const getpub = await publicIp.v4()
     const getloc = await internalIp.v4()
     console.log(`Server running server ip http://${getpub}:${process.env.PORT}/`);
     console.log(`Server running local development ip http://${getloc}:${process.env.PORT}/`);
+
+ 
 	})();
 });
